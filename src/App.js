3e0a8de37f00fec4bccbe20727d2d5ae91bafaef
Askar Akhmetuly <ASAKHM1975@GMAIL.COM>
2025-08-24 22:55:46 +0500

    Added logo to homepage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import './App.css';
+import logo from './logo.jpg'; // 👈 суретті src ішінен қосамыз
 
 function App() {
   return (
     <div className="App">
+      {/* Логотип */}
+      <img 
+        src={logo} 
+        alt="Meditation Logo" 
+        style={{ width: "200px", marginTop: "20px", borderRadius: "12px" }} 
+      />
+
+      {/* Тақырып */}
       <h1>Welcome to Meditation Paywall</h1>
     </div>
   );
